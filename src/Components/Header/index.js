8c6dda89045429cdee2a289c45b3937b755ffb38
Deepak Sisodiya <deepaksisodiya@Deepaks-MacBook-Pro.local>
2017-09-14 15:29:10 +0530

    created a header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header-container">
+        <h1>Flight Search Engine</h1>
+      </div>
+    );
+  }
+}
