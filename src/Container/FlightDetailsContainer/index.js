aa81900bfde7380d3f44811eab13e7ae437eded8
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2017-09-15 12:19:07 +0530

    render the flight details data dynamically

--- src/Container/FlightDetailsContainer/index.js
+++ src/Container/FlightDetailsContainer/index.js
@@ -20,59 +20,40 @@ export default class FlightDetailsContainer extends Component {
   }
 
   getAvailableFlights = (newProps) => {
+    const date = moment(newProps.departDateObj).format('DD/MM/YYYY');
     const flightDetailsArr = oneWayJson.filter((obj) => {
-      console.log(obj.departDate);
-      console.log(moment(newProps.departDate).format('DD/MM/YYYY'));
-      console.log(obj.departDate === moment(newProps.departDate).format('DD/MM/YYYY'));
       return (obj.originCity === newProps.originCity &&
-        obj.departDate === moment(newProps.departDate).format('DD/MM/YYYY') &&
+        obj.departDate ===  date &&
         obj.destinationCity === newProps.destinationCity
-      )
+      );
     });
+
     this.setState({
       flightDetailsArr,
     });
   };
 
   render() {
+    console.log('flightDetailsArr ', this.state.flightDetailsArr);
+    const { flightDetailsArr } = this.state;
     return (
       <div>
-        <FlightDetails
-          price="9000"
-          isOneWay={true}
-          flightNumber="AI-202"
-          originCity="PNQ"
-          destinationCity="DEL"
-          departTime="10 AM"
-          arriveTime="2 PM"
-        />
-        <FlightDetails
-          price="9000"
-          isOneWay={true}
-          flightNumber="AI-202"
-          originCity="PNQ"
-          destinationCity="DEL"
-          departTime="10 AM"
-          arriveTime="2 PM"
-        />
-        <FlightDetails
-          price="9000"
-          isOneWay={true}
-          flightNumber="AI-202"
-          originCity="PNQ"
-          destinationCity="DEL"
-          departTime="10 AM"
-          arriveTime="2 PM"
-        />
-        <FlightDetails
-          price="9000"
-          isOneWay={true}
-          flightNumber="AI-202"
-          originCity="PNQ"
-          destinationCity="DEL"
-          departTime="10 AM"
-          arriveTime="2 PM"
-        />
+        {flightDetailsArr.map((obj) => {
+
+          return (
+            <FlightDetails
+              price={obj.price}
+              isOneWay={true}
+              flightNumber={obj.flightNumber}
+              originCity="PNQ"
+              destinationCity="DEL"
+              departTime={obj.detaprtTime}
+              arriveTime={obj.arriveTime}
+            />
+          )
+
+        })}
+
       </div>
     )
   }
