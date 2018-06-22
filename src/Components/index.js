e08b4f285dae9b33061e7d035f5602ebd9a44d40
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-06-22 22:33:41 +0530

    Updates

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-import Header from './Header';
-import SubHeader from './SubHeader';
-import FlightDetails from './FlightDetails';
-
-export { Header, SubHeader, FlightDetails };
--- src/Container/index.js
+++
@@ -1,5 +0,0 @@
-import SearchForm from './SearchForm';
-import RangeSlider from './RangeSlider';
-import AvailableFlights from './AvailableFlights';
-
-export { SearchForm, RangeSlider, AvailableFlights };
